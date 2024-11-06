e3068618d00e6acc4876f958bcd9c52b11818b74
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-05 22:36:58 -0800

    index.html: - Answered the given questions in h2 elements.
    script.js:
    - Added five new numbers to string and outputted them to console.
    - Took away the last three numbers from the string using the "pop" command.
    - Outputted how many characters are in the phrase "Hello World!" to the console.

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ console.log(ArrayOfNumbers[ArrayOfNumbers.length - 1]); // Outputs only the firs
 for (let i = 0; i < ArrayOfNumbers.length; i++) {
     console.log(ArrayOfNumbers[i]);
 } // Outputs all five numbers from the string (7, 16, 21, 30, 38)
+
+ArrayOfNumbers.push(45, 56, 70, 84, 100);
+console.log(ArrayOfNumbers);
+
+ArrayOfNumbers.pop();
+ArrayOfNumbers.pop();
+ArrayOfNumbers.pop();
+console.log(ArrayOfNumbers);
+
+let RandomString = "Hello World!";
+console.log(RandomString.length);
